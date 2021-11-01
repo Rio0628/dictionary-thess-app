c477d2e3cd03f7136d4d42de08523003b3fcc00f
Rio0628 <mariomdomenech@gmail.com>
2021-11-01 15:51:09 -0400

    Created headbar
    - Created the headbar of the application with the main symbols of the "Logo" of the application, the searchbar for searching the meanings of word, and the login button which will change to the name of the user when they log in.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import searchIcon from './images/searchIcon.png';
 
 export default class App extends Component {
   render () {
     return (
       <div className='container'>
-        <h1>Hello</h1>
+        <div className='headbarContainer'>
+          <p className='appLogo'><span>Dicti</span>onary</p>
+        
+          <div className='searchbarContainer'>
+            <input type='text' placeholder='Enter a word...' className='searchbar'/>
+            <div className='searchbarBtn'><img src={searchIcon} alt='search button'/></div>
+          </div>
+
+          <div className='loginBtn'>Login</div>
+        </div>
+
+        
       </div>
     );
   }
