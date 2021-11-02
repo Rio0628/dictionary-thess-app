e6ecdeced8a659a0446d7d31a8fe851e0fcb6815
Rio0628 <mariomdomenech@gmail.com>
2021-11-01 21:28:00 -0400

    Created Sidebar
    - Created a simple sidebar for the application that will allow the user to  have different option apart from the main search function.
    - Added a box shadow for the application logo.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ export default class App extends Component {
     return (
       <div className='container'>
         <div className='headbarContainer'>
-          <p className='appLogo'><span>Dicti</span>onary</p>
-        
+          <p className='appLogo opened'><span>Dicti</span>onary</p>
           <div className='searchbarContainer'>
             <input type='text' placeholder='Enter a word...' className='searchbar'/>
             <div className='searchbarBtn'><img src={searchIcon} alt='search button'/></div>
           </div>
-
           <div className='loginBtn'>Login</div>
         </div>
-
+        
+       <div className='mainViewDctnry'>
+          <div className='sidebar'>
+            <p className='wordOfDay'>Word of The Day</p>
+            <p className='suggestWord'>Suggest a Word</p>
+            <p className='savedWords'>Saved Words</p>
+          </div>
+       </div>
         
       </div>
     );
