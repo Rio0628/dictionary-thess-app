9650b7e9c6cf84522bef39b4f3e0a7e5c691a77a
Rio0628 <mariomdomenech@gmail.com>
2021-11-03 16:45:20 -0400

    Created new state for button func.
    - Created a new state that will trigger the main component if it is true and remove it if it is false.
    - Functionality for the suggest word and the word of day button. Once the word of day button is clicked it will trigger the word of day component and set the main component state to false. When the suggest word component is triggered it will set the main comp state to true and display the main dictionary component. The app palette will also be reset to the dictionary view.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mainCompTriggered: true,
       sidebarTriggered: false, 
       dctnryTriggered: true,
       thsrsTriggered: false, 
+      wrdOfDayTriggered: false,
       wordSaved: false,
     };
   }
@@ -20,16 +22,29 @@ export default class App extends Component {
       console.log(e.target);
       
       if (e.target.className === 'appLogo') { this.setState({ sidebarTriggered: !this.state.sidebarTriggered }); }
+
       if (e.target.className === 'dctnryBtn' || e.target.className === 'suggestWord') {
         this.setState({ dctnryTriggered: true })
         this.setState({ thsrsTriggered: false }) 
         this.setState({ appPalette: '' });
       }
 
+      if (e.target.className === 'suggestWord') {
+        this.setState({ mainCompTriggered: true });
+        this.setState({ wrdOfDayTriggered: false });
+      }
+
       if (e.target.className === 'thsrsBtn') { 
         this.setState({ thsrsTriggered: true });
         this.setState({ dctnryTriggered: false })
         this.setState({ appPalette: 'thesaurus' });
+      }
+
+      if (e.target.className === 'wordOfDay') {
+        this.setState({ appPalette: '' });
+        this.setState({ thsrsTriggered: false });
+        this.setState({ mainCompTriggered: false })
+        this.setState({ wrdOfDayTriggered: true });
       }
     }
   
@@ -48,14 +63,21 @@ export default class App extends Component {
         </div>
           
         <div className='mainViewDctnry'>
+          
 
-          <MainWordView onClick={onClick} state={this.state.dctnryTriggered} palette={this.state.appPalette}/>
+          {this.state.wrdOfDayTriggered ? <WordOfTheDay onClick={onClick}/> : null }
+
+          {this.state.mainCompTriggered ? <MainWordView onClick={onClick} state={this.state.dctnryTriggered} palette={this.state.appPalette}/> : null}
+          
           {/* <WordOfTheDay onClick={onClick}/> */}
+          
           <div className={'sidebar active ' + this.state.appPalette}>
-              <p className='wordOfDay' onClick={onClick}>Word of The Day</p>
-              <p className='suggestWord' onClick={onClick}>Suggest a Word</p>
-              <p className='savedWords' onClick={onClick}>Saved Words</p>
+            <p className='wordOfDay' onClick={onClick}>Word of The Day</p>
+            <p className='suggestWord' onClick={onClick}>Suggest a Word</p>
+            <p className='savedWords' onClick={onClick}>Saved Words</p>
           </div>
+
+
         </div>
         
       </div>
