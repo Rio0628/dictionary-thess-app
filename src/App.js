01ffa5168bcfd3ec8b0e2dc229d0814927d04534
Rio0628 <mariomdomenech@gmail.com>
2021-11-03 23:36:22 -0400

    Created Saved Words Container
    - Created the saved words container element. Gave it its initial styling and positioning.
    - Populated the IndSavedWord component and placed it within the saved words container. Gave it its initial styling and passed the necessary onClick function.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,40 @@ import React, { Component } from 'react';
 import searchIcon from './images/searchIcon.png';
 import MainWordView from './components/MainWordView';
 import WordOfTheDay from './components/WordOfTheDay';
+import IndSavedWord from './components/IndSavedWord';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.wrapperRef = React.createRef();
+    this.navWrapperRef = React.createRef();
+    this.wrdsWrapperRef = React.createRef();
     this.state = {
       mainCompTriggered: true,
       sidebarTriggered: false, 
       dctnryTriggered: true,
       thsrsTriggered: false, 
       wrdOfDayTriggered: false,
-      wordSaved: false,
+      wordSaved: false, 
+      searchInput: '',
     };
   }
   
   render () {
+    let indSvdWrdContainer = [];
 
+    const onChange = (e) => {
+      // console.log(e.target.value)
+
+      if (e.target.className === 'searchbar') {
+        this.setState({ searchInput: e.target.value });
+      }
+    }
+    console.log(this.state.searchInput)
     const onClick = (e) => {
       console.log(e.target);
       
-      if (e.target.className === 'appLogo') {
-        const wrapper = this.wrapperRef.current;
+      if (e.target.className === 'appLogo' || e.target.className === 'wordOfDay' || e.target.className === 'suggestWord') {
+        const wrapper = this.navWrapperRef.current;
         wrapper.classList.toggle('active');   
       }
 
@@ -50,25 +62,43 @@ export default class App extends Component {
         this.setState({ mainCompTriggered: false })
         this.setState({ wrdOfDayTriggered: true });
       }
+
+      if (e.target.className === 'saveWrdBtn' || e.target.className === 'saveBtn') {
+        // words object { 'Word 1', 'Word 2', 'Word 3' } if word is not there then that 
+        this.setState({ wordSaved: true });
+      }
+
+      if (e.target.className === 'savedWords') {
+        const wrapper = this.wrdsWrapperRef.current;
+        wrapper.classList.toggle('active');
+      }
     }
 
-    console.log(this.state.appPalette)
-    console.log(this.state.thsrsTriggered)
+    for (let i = 0; i < 10; i++) {
+      indSvdWrdContainer.push( <IndSavedWord onClick={onClick} key={'word ' + i} /> );
+    }
+
+    // console.log(this.state.appPalette)
+    // console.log(this.state.thsrsTriggered)
 
     return (
       <div className='container'>
         <div className={'headbarContainer ' + this.state.appPalette}>
           <p className='appLogo' onClick={onClick}><span>Dicti</span>onary</p>
           <div className='searchbarContainer'>
-            <input type='text' placeholder='Enter a word...' className='searchbar'/>
-            <div className='searchbarBtn'><img src={searchIcon} alt='search button'/></div>
+            <input type='text' placeholder='Enter a word...' className='searchbar' onChange={onChange}/>
+            <div className='searchbarBtn' onClick={onClick}><img src={searchIcon} alt='search button'/></div>
           </div>
         </div>
           
-        <div ref={this.wrapperRef} className={'sidebar ' + this.state.appPalette}>
+        <div ref={this.navWrapperRef} className={'sidebar active ' + this.state.appPalette}>
             <p className='wordOfDay' onClick={onClick}>Word of The Day</p>
             <p className='suggestWord' onClick={onClick}>Suggest a Word</p>
             <p className='savedWords' onClick={onClick}>Saved Words</p>
+             
+            <div ref={this.wrdsWrapperRef} className={'savedWrdsContainer ' + this.state.appPalette}>
+              {indSvdWrdContainer}
+            </div>
         </div>
 
 
---
+++ src/components/IndSavedWord.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IndSavedWord = (props) => {
+    return (
+        <p className='indSavedWrd' onClick={props.onClick}>Saved Word</p>
+    )
+}
+
+export default IndSavedWord;
